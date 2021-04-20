97f13d7109303311cc47688b1bc579f5aba1ecfd
Manuel Alabor <manuel@alabor.me>
2021-04-20 18:20:30 +0200

    (ui) Implement DisposableContainer for IoC

--- src/ui/ioc.ts
+++ src/ui/ioc.ts
@@ -1,5 +1,6 @@
-import { Container } from 'inversify';
+import { Container, interfaces } from 'inversify';
 import 'reflect-metadata';
+import { IDisposable } from '../shared/types';
 import { ILogger } from './logger';
 import ConsoleLogger from './logger/consoleLogger';
 import TelemetryBridge, { ITelemetryBridge } from './telemetryBridge';
@@ -15,11 +16,32 @@ export function createRootContainer(): Container {
   return container;
 }
 
-export function createSessionContainer(parent: Container, cdpClientAddress: ICDPClientAddress): Container {
-  const container = new Container();
+export function createSessionContainer(parent: Container, cdpClientAddress: ICDPClientAddress): DisposableContainer {
+  const container = new DisposableContainer();
   container.parent = parent;
 
   container.bind<ICDPClientAddress>(ICDPClientAddress).toConstantValue(cdpClientAddress);
-  container.bind<ITelemetryBridge>(ITelemetryBridge).to(TelemetryBridge).inSingletonScope();
+  container
+    .bind<ITelemetryBridge>(ITelemetryBridge)
+    .to(TelemetryBridge)
+    .inSingletonScope()
+    .onActivation(container.trackDisposableBinding);
+
   return container;
+}
+
+class DisposableContainer extends Container implements IDisposable {
+  private readonly disposables: IDisposable[] = [];
+
+  trackDisposableBinding = <T extends IDisposable>(_context: interfaces.Context, injectable: T) => {
+    this.disposables.push(injectable);
+    return injectable;
+  };
+
+  dispose() {
+    for (const disposable of this.disposables) {
+      disposable.dispose();
+    }
+    this.unbindAll();
+  }
 }
