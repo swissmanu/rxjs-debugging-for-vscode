0d38068107e80474142e2245fcf452632a446033
Manuel Alabor <manuel@alabor.me>
2021-10-30 17:45:16 +0200

    Increase Async Test Timeout for CI

--- packages/extension-e2e/jest.config.js
+++ packages/extension-e2e/jest.config.js
@@ -1,7 +1,11 @@
+function isCI() {
+  return !!process.env['CI'];
+}
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   runner: 'vscode',
   modulePathIgnorePatterns: ['.vscode-test/'],
   testMatch: ['**/out/**/*.test.js'],
-  testTimeout: 20_000,
+  testTimeout: isCI() ? 50_000 : 20_000,
 };
