df9ae905a6ded3ca42da116ab64e6b7448dd44b7
Manuel Alabor <manuel@alabor.me>
2021-11-20 12:03:30 +0100

    Update Various Dependencies

--- packages/runtime/src/global.d.ts
+++ packages/runtime/src/global.d.ts
@@ -1,19 +1,18 @@
-import type {
-  CDP_BINDING_NAME_RUNTIME_READY,
-  CDP_BINDING_NAME_SEND_TELEMETRY,
-  RUNTIME_PROGRAM_ENV_VAR,
-  RUNTIME_TELEMETRY_BRIDGE,
-} from './consts';
+/* eslint-disable no-var */
 import type TelemetryBridge from './telemetryBridge';
 import { RuntimeType } from './utils/runtimeType';
 
+// Variable names MUST be kept in sync with ./const.ts!
 declare global {
-  namespace NodeJS {
-    interface Global {
-      [CDP_BINDING_NAME_RUNTIME_READY]: (x: RuntimeType) => void | undefined;
-      [CDP_BINDING_NAME_SEND_TELEMETRY]: (msg: string) => void | undefined;
-      [RUNTIME_TELEMETRY_BRIDGE]: TelemetryBridge | undefined;
-      [RUNTIME_PROGRAM_ENV_VAR]: string | undefined;
-    }
-  }
+  // CDP_BINDING_NAME_RUNTIME_READY:
+  var rxJsDebuggerRuntimeReady: (x: RuntimeType) => void | undefined;
+
+  //CDP_BINDING_NAME_SEND_TELEMETRY
+  var sendRxJsDebuggerTelemetry: (msg: string) => void | undefined;
+
+  // RUNTIME_TELEMETRY_BRIDGE
+  var rxJsDebuggerTelemetryBridge: TelemetryBridge | undefined;
+
+  //RUNTIME_PROGRAM_ENV_VAR
+  var RXJS_DEBUGGER_PROGRAM: string | undefined;
 }
